34d6d09993e78f489df10389b09c96daf164ecfe
yuezu1026 <39002818@qq.com>
2017-03-24 23:21:02 +0800

    add

---
+++ job.business.api/src/main/java/com/job/business/vo/AccountVO.java
@@ -0,0 +1,9 @@
+package com.job.business.vo;
+
+import java.io.Serializable;
+
+public class AccountVO implements Serializable{
+
+	private static final long serialVersionUID = 2528994766533655537L;
+
+}
